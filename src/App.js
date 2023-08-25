7cbf71a47c2b4130392a9bc18ec9a9c57097daf4
kema-ray <rachelkemuma99@gmail.com>
2023-08-25 07:37:03 +0300

    connect the client to react and fetch data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { gql, useQuery } from '@apollo/client';
 import './App.css';
+
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      id
+      title
+      author
+    }
+  }
+`;
+
+
+function DisplayBooks() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading....</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return data.books.map(({id, title, author}) => (
+    <div key={id}>
+      <h3>{title}</h3>
+      <p>{author}</p>
+    </div>
+  ));
+}
+
+// function Books({ onBookSelected }) {
+//   const { loading, error, data } = useQuery(GET_BOOKS);
+
+//   if (loading) return 'Loading...';
+//   if (error) return `Error! ${error.message}`;
+
+//   return (
+//     <select name='books' onChange={onBookSelected}>
+//       {data.books.map((book) => (
+//         <option key={book.id} value={book.title}>
+//           {book.title}
+//         </option>
+//       ))}
+//     </select>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Books List</h2>
+      <hr />
+      <DisplayBooks />
+      {/* <Books /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+// client.query({
+//   query : gql`
+//     query GetBooks {
+//       books {
+//         id
+//         title
+//         author
+//       }
+//     }
+//   `,
+// }).then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
